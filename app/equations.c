1e06b7a2c9ea3b4ddb032a1b5ea73e5133b0d876
Tomas Petras Rupsys <tprupsys@gmail.com>
2016-09-24 20:35:37 +0300

    Initial commit

---
+++ app/equations.c
@@ -0,0 +1,7 @@
+double schrodingerEquation() {
+    return 0.0; 
+}
+
+double findResultUsingGivenAlgorithm() {
+    return 0.0;
+}
---
+++ app/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <mpi.h>
+
+#include "equations.c"
+
+#define PARAMETER_A 2
+#define PARAMETER_C 3
+#define PARAMETER_D 4
+#define PARAMETER_K 1
+#define PARAMETER_ALPHA 1
+#define PARAMETER_BETA 2
+#define PARAMETER_GAMMA 1
+#define PARAMETER_THETA 2
+
+int main(int argc, char *argv[]) {
+    
+    MPI_Init(&argc, &argv);
+    return 0;
+}
